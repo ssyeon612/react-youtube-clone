ec603bce75b1917279b586ee7d9f799a0e3bb44b
ssyeon612 <ssyeon612@gmai.com>
2023-02-06 23:54:28 +0900

    add video upload api

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -37,6 +38,21 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value);
     };
 
+    const onDrop = (files) => {
+        let formData = new FormData();
+        const config = {
+            header: { "content-type": "multipart/form-data" },
+        };
+        formData.append("file", files[0]);
+        Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+            if (response.data.success) {
+                console.log(response.data);
+            } else {
+                alert("비디오 업로드를 실패했습니다.");
+            }
+        });
+    };
+
     return (
         <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
             <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -45,9 +61,9 @@ function VideoUploadPage() {
             <Form onSubmit>
                 <div style={{ display: "flex", justifyContent: "space-between" }}>
                     {/* Drop zone */}
-                    <Dropzone onDrop multiple maxSize>
+                    <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
                         {({ getRootProps, getInputProps }) => (
-                            <div style={{ width: "300px", height: "240px", border: "1px solid lightgrey", alignItems: "center", justifyContent: " center" }} {...getRootProps()}>
+                            <div style={{ width: "300px", height: "240px", border: "1px solid lightgrey", display: "flex", alignItems: "center", justifyContent: "center" }} {...getRootProps()}>
                                 <Input {...getInputProps()} />
                                 <Icon type="plus" style={{ fontSize: "3rem" }} />
                             </div>
